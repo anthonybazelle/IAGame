7502b959c9dbc9f4ccf7cc1f1494a564266ce02c
Anthony BAZELLE <a.bazelle1@laposte.net>
2018-05-18 01:56:35 +0200

    Random Training + Implem UCT a debug

--- Domineering/main.py
+++ Domineering/main.py
@@ -1,236 +1,250 @@
-"""
-# import os
-# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID" #see issue #152
-# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-
-import keras
-import numpy
-model = keras.models.Sequential()
-model.add(keras.layers.Dense(1, input_shape=(1,)))
-model.compile(optimizer=keras.optimizers.sgd(), loss=keras.losses.mse)
-
-X = [
-[1],
-[2]
-]
-
-Y = [
-[1],
-[3]
-]
-
-tb_callback = keras.callbacks.TensorBoard()
-
-model.fit(numpy.array(X), numpy.array(Y), callbacks=[tb_callback], epochs=2000)
-
-print(model.predict(numpy.array(X)))"""
-
 import numpy as np
 from random import randint
 
-sizeBoard = 9
-board = np.zeros((sizeBoard, sizeBoard))
+
+"""-------------------- GLOBAL VALUE ----------------------"""
+sizeBoard = 8
 rdmValue = np.zeros((sizeBoard, sizeBoard))
 moveHorizontal = []
 moveVertical = []
 hashTable = {}
-
-
-"""xor(bool(a), bool(b))"""
-
-
+bug = 0
 
 class State:
-    """ Correspond a un noeud de l'arbre """
-    """ param : coup joué pour cet état (donc cet hashcode) / total res / Nombre de passage / a qui le tour de jouer """
-    def __init__(self, move):
-        self.total = 0
-        self.passage = 1
-        self.move = move
-        self.mean = 0
-        self.turn = 0
-    
+	""" Correspond a un noeud de l'arbre """
+	""" param : coup joué pour cet état (donc cet hashcode) / total res / Nombre de passage / a qui le tour de jouer """
+	def __init__(self, move):
+		self.total = 0
+		self.passage = 1
+		self.move = move
+		self.mean = 0
+		self.turn = 0
 
-def setRandomValue(board):
-    for x in range(0, sizeBoard - 1):
-        for y in range(0, sizeBoard - 1):
-            rdmValue[x, y] = randint(0 , 9000000000000000000)
 
-def setHash(state):
-    hash = 0
-    for x in range(0, sizeBoard):
-        for y in range(0, sizeBoard):
-            if state[x, y] != 0:
-                piece = state[x, y]
-                hash ^= int(rdmValue[x, y])
-    return hash
+def setRandomValue():
+	for x in range(0, sizeBoard):
+		for y in range(0, sizeBoard):
+			rdmValue[x, y] = randint(0 , 9000000000000000000)
 
-"""
-def addToHashTable(state):
-    hash = findHash(state)
-        TODO : hashTable[Hash] = (mean, sumPlayouts, tabPlayouts[])
-    hashTable[hash] = (mean, sumPlayouts, [])
-"""
+def getHash(state):
+	hash = 0
+	for x in range(0, sizeBoard):
+		for y in range(0, sizeBoard):
+			if state[x, y] != 0:
+				piece = state[x, y]
+				hash ^= int(rdmValue[x, y])
+	return hash
 
 def clearBoard(state):
-    state = np.zeros((sizeBoard, sizeBoard))
+	state = np.zeros((sizeBoard, sizeBoard))
 
-def playout(state):
-    print("Playout")
-    hWin = 0
+def playout(boardParam):
+	print("Playout")
+	hWin = 0
 
-    """ 0 : Vertical   1 : Hortizontal """
-    playerTurn = 0
-    res = 0
-    legalMoves(state)
-    while len(moveHorizontal) > 0 and len(moveVertical) > 0:
-        pos = 0
-        legalMoves(state)
+	""" 0 : Vertical   1 : Hortizontal """
+	playerTurn = 0
+	res = 0
+	legalMoves(boardParam)
+	while len(moveHorizontal) > 0 and len(moveVertical) > 0:
+		pos = 0
+		legalMoves(boardParam)
 
-        move = []
-        if playerTurn == 0:
-            pos = randint(0, len(moveVertical) - 1)
-            move = moveVertical[pos]
-        elif playerTurn == 1:
-            pos = randint(0, len(moveHorizontal) - 1)
-            move = moveHorizontal[pos]
+		if len(moveHorizontal) == 0 or len(moveVertical) == 0:
+			break
+		move = []
+		player = 0
+		if playerTurn == 0:
+			"""print("Size moveVertical:")
+			print(len(moveVertical))"""
+			pos = randint(0, len(moveVertical))
+			"""print("Pos :")
+			print(pos)"""
+			move = moveVertical[pos - 1]
+			player = 0
+		elif playerTurn == 1:
+			"""print("Size moveHorizontal:")
+			print(len(moveHorizontal))"""
+			pos = randint(0, len(moveHorizontal))
+			"""print("Pos :")
+			print(pos)"""
+			move = moveHorizontal[pos - 1]
+			player = 1
 
-        state = play(state, move)
-        """"""
-        if playerTurn == 0:
-            playerTurn = 1
-        elif playerTurn == 1:
-            playerTurn = 0
+		play(boardParam, move, player)
+		""""""
+		if playerTurn == 0:
+			playerTurn = 1
+		elif playerTurn == 1:
+			playerTurn = 0
 
-    """"""
-    if len(moveVertical) == 0:
-        hWin = True
-    elif len(moveHorizontal) == 0:
-        hWin = False
+	""""""
+	if len(moveVertical) == 0:
+		hWin = True
+	elif len(moveHorizontal) == 0:
+		hWin = False
 
-    print(state)
-    return hWin
+
+	print(boardParam)
+	return hWin
 
 def legalMoves(state):
-    print("legalMove")
+	"""print("legalMove")"""
 
-    moveHorizontal.clear()
-    moveVertical.clear()
+	moveHorizontal.clear()
+	moveVertical.clear()
 
-    for x in range(0, sizeBoard):
-        adjacent = False
-        adjacentCase = ()
-        for y in range(0, sizeBoard):
-            if state[x, y] == 0 and adjacent == False:
-                adjacent = True
-                adjacentCase = (x, y)
-            elif state[x, y] == 1 or state[x, y] == 2:
-                adjacent = False
-            elif adjacent == True and state[x, y] == 0:
-                """print("Horizontal player can play")"""
-                moveHorizontal.append([adjacentCase, (x, y)])
-                adjacentCase = (x, y)
+	for x in range(0, sizeBoard):
+		adjacent = False
+		adjacentCase = ()
+		for y in range(0, sizeBoard):
+			if state[x, y] == 0 and adjacent == False:
+				adjacent = True
+				adjacentCase = (x, y)
+			elif state[x, y] == 1 or state[x, y] == 2:
+				adjacent = False
+			elif adjacent == True and state[x, y] == 0:
+				"""print("Horizontal player can play")"""
+				moveHorizontal.append([adjacentCase, (x, y)])
+				adjacentCase = (x, y)
 
-    for y in range(0, sizeBoard):
-        adjacent = False
-        adjacentCase = ()
-        for x in range(0, sizeBoard):
-            if state[x, y] == 0 and adjacent == False:
-                adjacent = True
-                adjacentCase = (x, y)
-            elif state[x, y] == 1 or state[x, y] == 2:
-                adjacent = False
-            elif adjacent == True and state[x, y] == 0:
-                """print("Vertical player can play")"""
-                moveVertical.append([adjacentCase, (x, y)])
-                adjacentCase = (x, y)
+	for y in range(0, sizeBoard):
+		adjacent = False
+		adjacentCase = ()
+		for x in range(0, sizeBoard):
+			if state[x, y] == 0 and adjacent == False:
+				adjacent = True
+				adjacentCase = (x, y)
+			elif state[x, y] == 1 or state[x, y] == 2:
+				adjacent = False
+			elif adjacent == True and state[x, y] == 0:
+				"""print("Vertical player can play")"""
+				moveVertical.append([adjacentCase, (x, y)])
+				adjacentCase = (x, y)
 
-    """
-    print("MoveHorizontal size :")
-    print(len(moveHorizontal))
-    print("MoveVertical size :")
-    print(len(moveVertical))
-    """
+"""
+	print("Size moveVert :")
+	print(len(moveVertical))
+	print("Size moveHoriz :")
+	print(len(moveHorizontal))"""
 
-def play(state, move):
-    """print("play")"""
-    itsOkay = False
-    itsVert = False
-    cpt = 0
-    oldX = 0
-    oldY = 0
+def play(boardParam, move, player):
+	"""print("play")"""
 
-    """print(state)"""
-    for (x, y) in move:
-        if cpt == 1:
-            if x == oldX:
-                itsVert = False
-            elif y == oldY:
-                itsVert = True
-        cpt += 1
+	for (x, y) in move:
+		if player == 0:
+			boardParam[x, y] = 1
+		elif player == 1:
+			boardParam[x, y] = 2
 
-    for (x, y) in move:
-        if itsVert == True:
-            state[x, y] = 1
-        elif itsVert == False:
-            state[x, y] = 2
+	hash = getHash(boardParam)
+	newStateToHash = State(move)
+	hashTable[hash] = newStateToHash
 
-    hash = setHash(state)
-    newStateToHash = State(move)
-    hashTable[hash] = newStateToHash
-    return state
-
-
+	return boardParam
 
 def seen(state):
-    print("seen")
-    hash = 0
-    for x in range(0, sizeBoard):
-        for y in range(0, sizeBoard):
-            if state[x, y] != 0:
-                piece = state[x, y]
-                hash ^= int(rdmValue[x, y])
-    return hashTable[hash]
+	print("seen")
+	global bug
+	hash = 0
+	for x in range(0, sizeBoard - 1):
+		for y in range(0, sizeBoard - 1):
+			if state[x, y] != 0:
+				piece = state[x, y]
+				hash ^= int(rdmValue[x, y])
+	if hash not in hashTable.keys():
+		bug += 1
+		return
+	return hashTable[hash]
 
-"""
-def uct(state):
-	if terminal(state):
-		return score(state)
-	moves = legalMoves(state)
-	t = seen(state)
-	if t!= None:
-	    t.sumPlayout -> nbPassageNoeudCourrant
-	    t.playouts[i] -> nbPassageNoeurFilsAIndexI
-	    
-	    for 
-		    best = argmax(t.mean[i] + c * sqrt(log(t.sumPlayouts) / t.playouts[i]))
-		state = play(state, moves[best])
-		res = UCT(state)
-		update t with res
+def selectBestMoveIndex(hashCurrentNode, currentNode):
+	moves = []
+	if currentNode.turn == 0:
+		moves = moveVertical
+	elif currentNode.turn == 1:
+		moves = moveHorizontal
+
+	maxValue = 0
+	indice = 0
+
+	"""argmax(t.mean[i] + c * sqrt(log(t.sumPlayouts) / t.playouts[i]))"""
+	for i in range(0, len(moves) - 1):
+		viergeBoard = np.zeros((sizeBoard, sizeBoard))
+		stateMove = play(viergeBoard, moves[i])
+		hashMove = getHash(stateMove)
+		hashChild = hashMove ^ hashCurrentNode
+		child = seen(hashChild)
+
+		if child == None:
+			print("NO CHILD IN SELECT BEST MOVE INDEX !")
+			child = State(stateMove)
+			hashTable[hashChild] = child
+
+		mean = child.total / child.passage
+		c = sqrt(2)
+		res = mean + c * sqrt(log(currentNode.passage) / child.passage)
+		if res > maxValue:
+			maxValue = res
+			indice = i
+
+	return indice
+
+
+def uct(boardParam, move):
+	"""if terminal(state):
+		return score(state)"""
+	legalMoves(boardParam)
+	t = seen(boardParam)
+	hashState = getHash(boardParam)
+
+	if t != None:
+		if t.turn == 0:
+			moves = moveVertical
+		elif t.turn == 1:
+			moves = moveHorizontal
+		best = selectBestMoveIndex(hashState, t)
+		boardParam = play(boardParam, moves[best])
+		res = UCT(boardParam, best)
+		if res == 0:
+			res = 1
+			t.total += res
+		else:
+			res = 0
 	else:
-		t = new entry in transposition table
-		res = playout(state)
-		update t with res
+		newHash = getHash(boardParam)
+		res = playout(boardParam)
+		newObj = State(move)
+		hashTable[newHash] = newObj
 	return res
-"""
 
 """ ----------------------- MAIN -----------------------"""
 
-setRandomValue(board)
-winner = playout(board)
-print(hashTable)
+setRandomValue()
+
+for i in range(0, 1000):
+	board = np.zeros((sizeBoard, sizeBoard))
+	winner = playout(board)
+
+print(len(hashTable))
+
+for i in range(0, 100):
+	board = np.zeros((sizeBoard, sizeBoard))
+	if seen(board) not in hashTable.keys():
+		newState = State(None)
+		hashTable[seen(board)] = newState
+	uct(board, selectBestMoveIndex(seen(board), hashTable[seen(board)]))
 
 """
 cpt = 0
 for i in range(0, 1000):
-    clearBoard(board)
-    winner = playout(board)
-    if winner == True:
-        print("Horizontal player win")
-        cpt += 1
-    else:
-        print("Vertical player win")
-    i = i + 1
+	clearBoard(board)
+	winner = playout(board)
+	if winner == True:
+		print("Horizontal player win")
+		cpt += 1
+	else:
+		print("Vertical player win")
+	i = i + 1
 
 print(cpt)
 meanH = cpt / 1000
